b572c91ede999c6eddf5fdcb74c1c804da3b8fca
Adrian Hirt <aedu.hirt@gmail.com>
2020-09-19 23:18:14 +0200

    Some stuff with masks and such. No corona here.

--- find_nodes.py
+++ find_nodes.py
@@ -4,8 +4,10 @@ from matplotlib import pyplot as plt
 import glob, os
 import math
 
-black_out_nodes = False
-add_legend = True
+black_out_nodes = True # Set this to true to black out the found nodes
+add_legend = False # Set this to true to label the found nodes
+generate_labels = False # Set this to true to label an unlabeled image
+
 
 def posDist(a, b):
   return math.sqrt(pow((a[0]-b[0]), 2) + pow((a[1]-b[1]), 2))
@@ -53,24 +55,44 @@ def match(img_gray, img_rgb, templateName, threshold, rotMode):
       print("270 deg")
       matchImage(img_gray, img_rgb, rot270, templateName, threshold)
 
-img_rgb = cv.imread('ex4.png')
-img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
+if generate_labels:
+  img_rgb = cv.imread('ex4.png')
+  img_gray = cv.cvtColor(img_rgb, cv.COLOR_BGR2GRAY)
 
-os.chdir("symbols")
-for file in glob.glob("high_t/*.png"):
-  match(img_gray, img_rgb, file, 0.88, 2)
+  os.chdir("symbols")
+  for file in glob.glob("high_t/*.png"):
+    match(img_gray, img_rgb, file, 0.88, 2)
 
-for file in glob.glob("low_t/*.png"):
-  match(img_gray, img_rgb, file, 0.8, 2)
+  for file in glob.glob("low_t/*.png"):
+    match(img_gray, img_rgb, file, 0.8, 2)
 
-for file in glob.glob("low_t/rot_inv/*.png"):
-  match(img_gray, img_rgb, file, 0.8, 0)
+  for file in glob.glob("low_t/rot_inv/*.png"):
+    match(img_gray, img_rgb, file, 0.8, 0)
 
-for file in glob.glob("low_t/rot_once/*.png"):
-  match(img_gray, img_rgb, file, 0.8, 1)
+  for file in glob.glob("low_t/rot_once/*.png"):
+    match(img_gray, img_rgb, file, 0.8, 1)
 
 
-#match(img_gray, img_rgb, "lines/Crossing.png", (r, g, b), 0.8, 1)
-#match(img_gray, img_rgb, "lines/Junction.png", (r, g, b), 0.82, 2)
+  #match(img_gray, img_rgb, "lines/Crossing.png", (r, g, b), 0.8, 1)
+  #match(img_gray, img_rgb, "lines/Junction.png", (r, g, b), 0.82, 2)
 
-cv.imwrite('../res.png', img_rgb)
+  cv.imwrite('../res.png', img_rgb)
+
+black = (0, 0, 0)
+grey = (100, 100, 100)
+white = (255, 255, 255)
+
+res_img = cv.imread('res.png')
+hsv_img = cv.cvtColor(res_img, cv.COLOR_RGB2HSV)
+line_mask = cv.inRange(hsv_img, grey, white)
+node_mask = cv.inRange(hsv_img, black, grey)
+
+kernel = np.ones((10, 10), np.uint8)
+dilated_line_mask = cv.dilate(line_mask, kernel, iterations=1)
+
+meeting_points = (node_mask & dilated_line_mask)
+
+cv.imwrite('line_mask.png', line_mask)
+cv.imwrite('node_mask.png', node_mask)
+cv.imwrite('line_mask_dilated.png', dilated_line_mask)
+cv.imwrite('meeting_points.png', meeting_points)

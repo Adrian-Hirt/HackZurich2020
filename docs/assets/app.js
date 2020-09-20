4fb7239162ecb378d8ca3ab6406d024b4513f864
Pascal Wacker <pascal.wacker@gmx.ch>
2020-09-20 04:11:31 +0200

    Example 2 data and gifs

--- docs/assets/app.js
+++ docs/assets/app.js
@@ -1,8 +1,8 @@
        
 // set the dimensions and margins of the graph
 var margin = {top: 10, right: 30, bottom: 30, left: 40},
-width = Math.max(500, window.innerWidth/2) - margin.left - margin.right,
-height = Math.max(500, window.innerHeight/2) - margin.top - margin.bottom;
+width = Math.max(1500, window.innerWidth/2) - margin.left - margin.right,
+height = Math.max(1500, window.innerHeight/2) - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#my_dataviz")
